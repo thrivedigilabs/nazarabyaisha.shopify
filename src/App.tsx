75f93e4c0a601b0ba9da481f872ecfee61217ea4
ThriveDigiLabs <thrivedigilabs@gmail.com>
2025-11-16 11:44:17 +0530

    1

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/nazarabyaisha.shopify">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/collections" element={<Collections />} />
